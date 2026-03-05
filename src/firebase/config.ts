fc7dba631bf15e3cc62201d83050923379864877
Nevin <fittconnect2@gmail.com>
2026-03-05 17:45:01 +0000

    Got this error please create a bucket
    ## Error Type
    Runtime FirebaseErr

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-8415098687-51f42",
   "appId": "1:128183447694:web:9eec23edb5c2c28d3919df",
+  "storageBucket": "studio-8415098687-51f42.appspot.com",
   "apiKey": "AIzaSyBe1DwjrhQUWcWxSPMhobMtiSQCAmiawME",
   "authDomain": "studio-8415098687-51f42.firebaseapp.com",
   "measurementId": "",
